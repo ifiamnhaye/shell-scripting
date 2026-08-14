8485f6a04ed07c096894408e53435c827219b901
ifiamnhaye <ifi.amn.haye@gmail.com>
2026-08-14 16:21:03 -0500

    add first file hello.sh

---
+++ homework/task-00/submission-homework/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash    #shebang statment
+
+#
+#first program of hello
+# Script Name : hello.sh
+# Description : A simple script to print a greeting message.
+# Usage       : ./hello_devops.sh
+echo "Hello,Devops!"  #print the hello,devops
+
