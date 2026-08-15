0c1e4fd83804ce7bc42657a4d5b741f48790dc77
ifiamnhaye <ifi.amn.haye@gmail.com>
2026-08-15 02:48:38 -0500

    modify hello.sh and add hello1.sh

--- homework/task-00/submission-homework/hello.sh
+++ homework/task-00/submission-homework/hello.sh
@@ -4,6 +4,6 @@
 #first program of hello
 # Script Name : hello.sh
 # Description : A simple script to print a greeting message.
-# Usage       : ./hello_devops.sh
+# Usage       : ./hello.sh
 echo "Hello,Devops!"  #print the hello,devops
 
---
+++ homework/task-00/submission-homework/hello1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#
+#
+#
+#
+# OUTPUT HELLO STATEMENTS
+# ------------------------------------------------------------------------------
+# Print a standard greeting to standard output
+echo "=========================================="
+echo "          HELLO, DEVOPS ENGINEER!         "
+echo "=========================================="
