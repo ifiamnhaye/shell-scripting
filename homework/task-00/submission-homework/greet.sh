b787d373cef7c337870d0ade51cf6c10f3c82fa1
ifiamnhaye <ifi.amn.haye@gmail.com>
2026-08-16 08:03:41 -0500

    add greet.sh

---
+++ homework/task-00/submission-homework/greet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Prompt user for their name
+read -r -p "Enter your name: " name
+echo
+# prompt user for their favorite tool
+read -r -p "Enter your favorite tool: " tool
+echo
+# Print variables output statment
+echo "Hello $name, your favorite tool is $tool."
+# end of the program
